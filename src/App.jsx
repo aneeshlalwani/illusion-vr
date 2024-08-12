c13b935f45d65a044997b37f56842224bec421bb
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-08-12 19:00:59 +0500

    implemented workflow component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import WorkFlow from "./components/WorkFlow";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero />
         <Features />
+        <WorkFlow />
       </div>
     </>
   );
